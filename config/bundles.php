0945552d0787a4cdb9fdcaf02994dcee097923e7
akira <akserikawa@gmail.com>
2019-01-03 21:09:01 +0100

    Update bundles & configuration

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
